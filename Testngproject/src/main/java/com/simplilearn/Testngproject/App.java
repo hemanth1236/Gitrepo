1b2123f34f7655ab45eb72ce600c6bafde38487f
hemanth1236 <41285003+hemanth1236@users.noreply.github.com>
2024-01-21 21:18:57 +0530

    Add files via upload

---
+++ Testngproject/src/main/java/com/simplilearn/Testngproject/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Testngproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
